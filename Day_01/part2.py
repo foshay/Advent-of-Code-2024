21a72194b7f2eb3688def6e8b78ad6871583f804
foshay <isak@teamjck.com>
2024-12-20 13:30:05 -0800

    Refactor part2.py to read from input.txt and implement logic for processing data with regex

--- Day_01/part2.py
+++ Day_01/part2.py
@@ -1,6 +1,31 @@
+import re
 
-with open('test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     inp = f.readlines()
+    leftSide = []
+    rightSide = {}
     for l in inp:
-        print(l)
+        pattern = r'(\d+)\s{3}(\d+)'
+        #print(l)
         l = l.replace('\n', '')
+        theMatch = re.match(pattern, l)
+
+        if theMatch:
+            x = theMatch.group(1)
+            y = theMatch.group(2)
+            #print(x, y)
+            leftSide.append(int(x))
+            if int(y) in rightSide:
+                #print("y is in rightSide")
+                rightSide[int(y)] = rightSide[int(y)] + 1
+            else:
+                rightSide[int(y)] = 1
+    #print(leftSide)
+    #print(rightSide)
+    total = 0
+    for i in leftSide:
+        if i in rightSide:
+            #print(i, rightSide[i])
+            total += (i * rightSide[i])
+    print(total)
+    
